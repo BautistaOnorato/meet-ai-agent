bd64ac8b860c27008548f3808ec59644c4dbb013
BautistaOnorato <bautistaonorato27@gmail.com>
2025-07-08 13:14:27 -0300

    next + shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div >
+    <div className="flex justify-center items-center h-screen text-4xl font-bold text-green-400">
       Boca
     </div>
   );
